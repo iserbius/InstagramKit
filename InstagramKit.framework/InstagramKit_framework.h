3e0768c9ab323583e916992cda169fec188aeb1e
Andrey Samoshkin <crees007@rambler.ru>
2018-03-02 11:31:28 +0300

    Add Carthage

---
+++ InstagramKit.framework/InstagramKit_framework.h
@@ -0,0 +1,19 @@
+//
+//  InstagramKit_framework.h
+//  InstagramKit.framework
+//
+//  Created by Handh on 02.03.2018.
+//  Copyright © 2018 InstagramKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstagramKit_framework.
+FOUNDATION_EXPORT double InstagramKit_frameworkVersionNumber;
+
+//! Project version string for InstagramKit_framework.
+FOUNDATION_EXPORT const unsigned char InstagramKit_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramKit_framework/PublicHeader.h>
+
+
